e20734a04381f4f45cf4464368afa3b7b925949b
linghao518 <tonysh518@aliyun.com>
2016-04-08 22:13:43 +0800

    update

--- api/image/image_controller.js
+++ api/image/image_controller.js
@@ -1,12 +1,11 @@
 var im = require('imagemagick');
 
-exports.covert = function(src, cb) {
-    console.log(src);
+exports.covert = function(file, cb) {
     im.resize({
-        srcPath: src,
-        dstPath: './pics/1.jpg',
+        srcPath: file.path,
+        dstPath: './pics/' + file.filename + '.jpg',
         width: 1000
-    }, function(err, stdout, stderr){
+    }, function(err, stdout, stderr) {
         if (err) throw err;
         cb && cb();
     });
--- api/index.js
+++ api/index.js
@@ -2,29 +2,37 @@ var express = require('express');
 var path = require('path')
 var multer = require('multer');
 var imageCtr = require('./image/image_controller');
+var db = require('./db/db');
 
 
 var app = express();
 
-var upload = multer({ dest: 'uploads' }).single('avatar');
+var upload = multer({ dest: 'uploads' }).single('image');
 
 
 app.post('/image', function(req, res, next) {
     upload(req, res, function(err) {
         next();
     });
-  }, function(req, res, next) {
-        imageCtr.covert(req.file.path, function() {
-            next();
-        });
-  }, function(req, res) {
-        res.json({
-            result: 'success'
-        });
+}, function(req, res, next) {
+    imageCtr.covert(req.file, function() {
+        next();
+    });
+}, function(req, res) {
+    res.json({
+        result: 'success'
+    });
 });
 
-/* istanbul ignore next */
-if (!module.parent) {
-    app.listen(3000);
-    console.log('Express started on port 3000');
-}
+
+// Connect to Mongo on start
+db.connect('mongodb://localhost:27017/msepin', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  } else {
+    app.listen(3000, function() {
+      console.log('Listening on port 3000...')
+    })
+  }
+})
